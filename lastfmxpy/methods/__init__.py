0f0011971dd3a8a8e939b0b6d3abe932e7866d10
pkeorley <nerfinyt@gmail.com>
2024-01-06 11:17:07 +0100

    fix(lastfmxpy/methods): Fix imports

--- lastfmxpy/methods/__init__.py
+++ lastfmxpy/methods/__init__.py
@@ -3,15 +3,15 @@
 # This source code is licensed under the MIT license found in the LICENSE
 # file in the root directory of this source tree.
 
-from .album import Album
-from .artist import Artist
-from .auth import Auth
-from .chart import Chart
-from .geo import Geo
-from .library import Library
-from .tag import Tag
-from .track import Track
-from .user import User
+from lastfmxpy.methods.album import Album
+from lastfmxpy.methods.artist import Artist
+from lastfmxpy.methods.auth import Auth
+from lastfmxpy.methods.chart import Chart
+from lastfmxpy.methods.geo import Geo
+from lastfmxpy.methods.library import Library
+from lastfmxpy.methods.tag import Tag
+from lastfmxpy.methods.track import Track
+from lastfmxpy.methods.user import User
 
 __all__ = [
     "Album",
