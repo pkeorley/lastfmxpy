323fd7789851d7e070bd8d2b02bbf82988dd0b3e
pkeorley <nerfinyt@gmail.com>
2024-01-06 11:04:51 +0100

    fix(lastfmxpy): Set requirements

--- setup.py
+++ setup.py
@@ -3,15 +3,25 @@
 # This source code is licensed under the MIT license found in the LICENSE
 # file in the root directory of this source tree.
 
+import pathlib
 from setuptools import setup, find_packages
+import pkg_resources
 
-# Read the contents of your README file
-with open('README.md', 'r', encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(
     name='lastfmxpy',
-    version='1.0.1',
+    version='1.0.2',
+    python_requires='>=3.10',
+    install_requires=install_requires,
     packages=find_packages(exclude=['.venv']),
     url='https://github.com/pkeorley/lastfmxpy',
     license='MIT',
